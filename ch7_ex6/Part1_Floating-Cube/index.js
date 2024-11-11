73a79d8c49e673f3ac0e4da8ae8ff7b02b29f8ce
ntvdavid <thienvu11150@gmail.com>
2024-11-11 13:01:01 +0100

    Three-Installation

--- ch7_ex6/Part1_Floating-Cube/index.js
+++ ch7_ex6/Part1_Floating-Cube/index.js
@@ -1,5 +1,5 @@
-import * as THREE from "../../node_modules/three/build/three.module.js";
-
+//import * as THREE from "../../node_modules/three/build/three.module.js";
+import * as THREE from 'three';
 // global scene values
 var btn, gl, glCanvas, camera, scene, renderer, cube;
 
